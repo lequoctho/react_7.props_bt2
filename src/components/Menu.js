7541b709cc87f6d257b1b0b54590c15a428a34cc
Glitch (starter-react-template) <none>
2020-05-23 09:02:17 +0000

    🍞😚 Checkpoint ./src/style/Menu.css:15532837/346 ./src/components/Menu.js:15532837/17

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,7 +6,7 @@ class Menu extends Component {
     render() {
         return <div class="menu">
                 <img src="https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111"/>
-                <p>HOME</p>
+                <p class="text">HOME</p>
               </div>;
     }
 }
