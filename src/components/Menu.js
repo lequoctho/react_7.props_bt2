2aaf5f1aba02e9796cd4c9cf69da5b50ac5d7d5a
Glitch (starter-react-template) <none>
2020-05-23 08:15:11 +0000

    🍰👲 Checkpoint ./src/App.js:15532837/497 ./src/components/menu.js:15532837/231 ./src/components/Menu.js:15532837/44

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Menu extends Component {
+    render() {
+        return <div class="menu">
+                
+              </div>;
+    }
+}
+
+export default Menu;
