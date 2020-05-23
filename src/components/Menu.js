97ccec6a794ef90a265f10850b643617e597dc99
Glitch (starter-react-template) <none>
2020-05-23 08:50:22 +0000

    🍤💏 Checkpoint ./.glitch-assets:15532837/349 ./src/components/Menu.js:15532837/178 ./src/style/Menu.css:15532837/158

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,8 @@ import '../style/Menu.css'
 class Menu extends Component {
     render() {
         return <div class="menu">
-                <i class="fas fa-home">test</i>
+                <img src="https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111"/>
+                <p>HOME</p>
               </div>;
     }
 }
