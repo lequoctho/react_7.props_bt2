2262c27d4cfc3297a59e1972b1c236a8117b0210
Glitch (starter-react-template) <none>
2020-05-23 09:59:51 +0000

    👼🎎 Checkpoint ./src/App.js:15532837/1876

--- src/App.js
+++ src/App.js
@@ -5,14 +5,37 @@ import TabMenu from './components/TabMenu';
 
 class App extends Component {
   render() {
+    const icons = [
+        {
+          icon: 'https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111', 
+          label: 'Home'
+        },
+        {
+          icon: 'https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111', 
+          label: 'Home'
+        },
+        {
+          icon: 'https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111', 
+          label: 'Home'
+        },
+        {
+          icon: 'https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111', 
+          label: 'Home'
+        },
+        {
+          icon: 'https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111', 
+          label: 'Home'
+        },
+      ];
     return (
-      
       <div className="App">
-        <TabMenu title="Home" image="https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111"/>
-        <TabMenu title="Home" image="https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111"/>
-        <TabMenu title="Home" image="https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111"/>
-        <TabMenu title="Home" image="https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111"/>
-        <TabMenu title="Home" image="https://cdn.glitch.com/6a47dbff-a645-429e-bc55-3fb2a767e153%2Fhome.png?v=1590223564111"/>
+        // {icons.map(x=>{
+        //   <TabMenu title={x.icon} image={x.label}/>
+        // })};
+        {icons.map(x => (
+        <TabMenu title={x.icon} image={x.label}/>
+      ))}
+        
       </div>
     );
   }
